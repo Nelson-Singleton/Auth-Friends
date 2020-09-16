c9b0282e09ba9338c0d08ab71e1a5042c00048b2
nelson-singleton <nsingleton@ymail.com>
2020-09-15 21:31:46 -0500

    Built and exported PrivateRoute. Finished form component.

--- friends/src/Components/login.js
+++ friends/src/Components/login.js
@@ -1,4 +1,6 @@
+import Axios from "axios"
 import React, { useState } from "react"
+import axiosWithAuth from '../Utils/axiosWithAuth'
 
 //states
 const [creds, setCreds] = useState({
@@ -8,25 +10,38 @@ const [isLoading, setIsLoading] = useState(false)
 
 //functions
 const handleChange = e => {
+    setCreds({
+        credentials:
+            {...creds,
+            [e.target.name]: e.target.value
+    }
+    })
+}
+
+const login = e => {
+    axiosWithAuth
+        .post('/api/login', creds.credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.token) //.payload
+            //history.push('/protected')
+        })
 
 }
 
 const LoginForm = () => {
     return(
         <div>
-            <form>
+            <form onSubmit = {login}>
                 <input
                 type = 'text'
-                name = 'username'
-                placeholder = 'username'
+                name = 'username'                
                 value = {creds.credentials.username}
                 onChange = {handleChange}
                 />
 
                 <input
                 type = 'text'
-                name = 'password'
-                placeholder = 'password'
+                name = 'password'                
                 value = {creds.credentials.password}
                 onChange = {handleChange}
                 />
